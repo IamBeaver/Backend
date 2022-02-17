6649c19e1bd47515b34806aa8465def2e115db19
IamBeaver <igor.bobro.work@gmail.com>
2022-02-17 15:57:16 +0200

    created db context

---
+++ Backend/DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Data.Configurations;
+using DataAccess.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserEntityTypeConfiguration());
+        }
+    }
+}
