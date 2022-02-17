c5be609c5b0bc38c9576ed1cc4b38c8de3033dea
IamBeaver <igor.bobro.work@gmail.com>
2022-02-17 15:57:31 +0200

    created configuration for user

---
+++ Backend/DataAccess/Data/Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Data.Configurations
+{
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseHiLo("user_hilo").IsRequired();
+            builder.Property(x => x.FirstName).HasMaxLength(30).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(30).IsRequired();
+            builder.Property(x => x.Age).IsRequired();
+        }
+    }
+}
