fb33d75f38827e67c012fb8e58acaa4938d54694
IamBeaver <igor.bobro.work@gmail.com>
2022-02-18 15:39:56 +0200

    Create users service and inject it

---
+++ Backend/Backend/Services/BaseDataService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Services
+{
+    public abstract class BaseDataService<T>
+        where T : DbContext
+    {
+        private readonly T _dbContext;
+        private readonly ILogger<T> _logger;
+
+        public BaseDataService(
+            T dbContext,
+            ILogger<T> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        protected async Task<T1> ExecuteSafe<T1>(Func<Task<T1>> action)
+        {
+            using (var transaction = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var result = await action();
+                    transaction.Commit();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    _logger.LogError(ex, "Transaction is rollbacked");
+                    return default(T1) !;
+                }
+            }
+        }
+    }
+}
---
+++ Backend/Backend/Services/UsersService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Backend.Models.Dtos;
+using Backend.Repository.Interfaces;
+using Backend.Services.Interfaces;
+using DataAccess.Data;
+
+namespace Backend.Services
+{
+    public class UsersService : BaseDataService<ApplicationDbContext>, IUsersService
+    {
+        private readonly IUsersRepository _usersRepository;
+        private readonly IMapper _mapper;
+        public UsersService(
+            IUsersRepository usersRepository,
+            IMapper mapper,
+            ApplicationDbContext dbContext,
+            ILogger<ApplicationDbContext> logger)
+            : base(dbContext, logger)
+        {
+            _usersRepository = usersRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> CreateUserAsync(BaseUserDto userDto)
+        {
+            return await ExecuteSafe(async () =>
+            {
+                return await _usersRepository.Create(userDto);
+            });
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            return await ExecuteSafe(async () =>
+            {
+                return await _usersRepository.Delete(id);
+            });
+        }
+
+        public async Task<IEnumerable<UserDto>> GetUsersAsync()
+        {
+            return await ExecuteSafe(async () =>
+            {
+                return _mapper.Map<IEnumerable<UserDto>>(await _usersRepository.GetUsers());
+            });
+        }
+
+        public async Task<bool> UpdateUserAsync(UserDto userDto)
+        {
+            return await ExecuteSafe(async () =>
+            {
+                return await _usersRepository.Update(userDto);
+            });
+        }
+    }
+}
